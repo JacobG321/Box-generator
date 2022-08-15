420eb0a989f3e3046ed54563a0637c4ec165216c
JacobG321 <jacobgruber1998@gmail.com>
2022-08-15 16:16:00 -0400

    finished box generator

---
+++ src/components/Box.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../static/Box.module.css';
+
+const Box = () => {
+  return (
+    <div className={styles.box}></div>
+  )
+}
+
+export default Box
---
+++ src/components/Form.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import styles from '../static/Form.module.css';
+
+const Form = () => {
+    
+    const [color, setColor] = useState('')
+    const [newColor, setNewColor] = useState([])
+
+    const createColor = (e) => {
+        e.preventDefault();
+        
+        const newAddition = {color}
+        setNewColor([...newColor, newAddition])
+        setColor('')
+    }
+
+
+    const handleColor = (e) => {
+        setColor(e.target.value)
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={createColor}>
+                <label htmlFor="color">Color</label>
+                <input type="text" name="color" value={color} onChange={handleColor}/>
+                <button type='submit'>Add</button>
+            </form>
+            <p>{JSON.stringify(newColor)}</p>
+
+            <div className={styles.boxes}>
+                {
+                    newColor.map((item, index)=>
+                        <div className={styles.box} key={index} style={{backgroundColor:item.color}}>
+                            {item.color}
+                        </div>
+                    )
+                }
+            </div>
+            
+        </div>
+    )
+}
+
+export default Form
